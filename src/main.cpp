c68592960e294cfaab7c490811c33e4f47e91ca8
Joe <jintorre@gmail.com>
2018-06-02 08:46:53 -0400

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	
+  //int i,j;
+  //i = 6;
+  //string z;
+  //std::cout << "Hello, MINGW!" << std::endl;
+  //std::cin >> i;
+  //std::getline(std::cin, z);
+  return 0;
+}
